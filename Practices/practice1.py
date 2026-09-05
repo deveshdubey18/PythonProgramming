c63fad46f173949df1e7e355ec2f807271212243
Devesh R. Dubey <314devesh11scic@gmail.com>
2026-09-05 14:40:40 +0530

    Add Python interview questions for even/odd and largest number

---
+++ Practices/practice1.py
@@ -0,0 +1,28 @@
+# 🐍 Python Interview Questions
+
+## Check whether a number is even or odd
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+## Find the largest of three numbers without max()
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+
+print("Largest number:", largest)
+
+
