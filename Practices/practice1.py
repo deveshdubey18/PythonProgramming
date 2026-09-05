0e479bbaabc42c5e2ba5542ea6a76239763be4be
Devesh R. Dubey <314devesh11scic@gmail.com>
2026-09-05 14:48:35 +0530

    Implement various number algorithms in practice1.py
    Added implementations for factorial, Fibonacci series, number reversal, and palindrome check.

--- Practices/practice1.py
+++ Practices/practice1.py
@@ -10,6 +10,8 @@ else:
     print("Odd")
 
 
+
+
 ## Find the largest of three numbers without max()
 
 a = int(input("Enter first number: "))
@@ -24,5 +26,70 @@ else:
     largest = c
 
 print("Largest number:", largest)
+
+
+
+
+## Factorial using a loop
+
+num = int(input("Enter a number: "))
+factorial = 1
+# using for loop 
+for i in range(1, num + 1):
+    factorial = factorial * i
+print("Factorial:", factorial)
+# using while loop
+while num>1:
+    factorial = factorial*num
+    num = num-1
+print("Factorial:", factorial)
+
+
+
+## Fibonacci series up to n terms
+n = int(input("Enter number of terms: "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    
+    a, b = b, a + b
+
+
+
+
+## Reverse a number
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reversed number:", reverse)
+
+
+
+
+## Check whether a number is palindrome
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
 
 
