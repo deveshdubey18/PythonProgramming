e493447e80172f353c55a3b32b74297928da4644
Devesh R. Dubey <314devesh11scic@gmail.com>
2026-09-07 21:01:29 +0530

    Document string, dictionary, and set methods
    Added examples of string, dictionary, and set methods in Python.

--- Practices/practice2.py
+++ Practices/practice2.py
@@ -14,3 +14,82 @@ index() - Find index
 count() - Count occurrences
 '''
 
+## String Methods
+text = "Hello World"
+
+'''
+text.lower()
+text.upper()
+text.strip()
+text.replace("Hello", "Hi")
+text.split()
+text.find("World")
+text.count("l")
+text.startswith("Hello")
+text.endswith("World")
+"is123".isalnum()
+"123".isdigit()
+"hello".isalpha()
+'''
+
+
+## Dictionary Methods
+data = {
+    "name": "Devesh",
+    "age": 22
+}
+
+'''
+data.keys()
+data.values()
+data.items()
+data.get("name")
+data.update({"age": 23})
+data.pop("age")
+'''
+
+## Set Methods
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+'''
+a.union(b)
+a.intersection(b)
+a.difference(b)
+
+output- 
+Union        → {1,2,3,4,5}
+Intersection → {3}
+Difference   → {1,2}
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
