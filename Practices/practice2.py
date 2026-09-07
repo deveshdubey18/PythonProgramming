01b6adcd579827871de5bbafc53659eeb1bfd6c4
Devesh R. Dubey <314devesh11scic@gmail.com>
2026-09-07 20:56:51 +0530

    Create practice2.py with Python list methods
    Added documentation for Python DSA and list methods.

---
+++ Practices/practice2.py
@@ -0,0 +1,19 @@
+## Python DSA Methods
+
+## Python List Methods
+list = [10,20,40]
+''' 
+append() - Add element
+extend() - Add multiple elements
+insert() - Insert at position
+remove() - Remove value
+pop() - Remove by index
+sort()
+Sort list
+reverse()
+Reverse list
+index()
+Find index
+count()
+Count occurrences
+'''
