0a9583ff87687af5f14c99b88dbf8b78a1ca46bb
Devesh R. Dubey <314devesh11scic@gmail.com>
2026-09-08 19:50:52 +0530

    Update practice2.py

--- Practices/practice2.py
+++ Practices/practice2.py
@@ -66,6 +66,19 @@ Difference   → {1,2}
 
 
 
+## Linear Search
+numbers = [10, 20, 30, 40, 50]
+
+target = 30
+
+for i in range(len(numbers)):
+    if numbers[i] == target:
+        print("Found at index:", i)
+        break
+
+
+
+
 
 
 
